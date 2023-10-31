33febc35a8e948136e852fd1a62dc2ea8b01611b
Felicia Chen <feliciachen18@gmail.com>
2023-10-31 14:22:23 -0700

    added files

---
+++ MouseEvents.h
@@ -0,0 +1,14 @@
+//
+// Created by Felicia Chen on 10/31/23.
+//
+
+#ifndef TEXTINPUT_MOUSEEVENTS_H
+#define TEXTINPUT_MOUSEEVENTS_H
+
+
+class MouseEvents {
+
+};
+
+
+#endif //TEXTINPUT_MOUSEEVENTS_H
